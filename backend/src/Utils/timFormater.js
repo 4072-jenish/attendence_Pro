281f27456a11f7f51c599b5c77c9903532b93b0b
Jenish-Hariyani <150036720+4072-jenish@users.noreply.github.com>
2026-02-26 15:02:39 +0530

    Update timFormater.js

--- backend/src/Utils/timFormater.js
+++ backend/src/Utils/timFormater.js
@@ -2,6 +2,7 @@ exports.formatTime = (date) => {
   return new Date(date).toLocaleTimeString('en-IN', {
     hour: '2-digit',
     minute: '2-digit',
-    hour12: false
+    hour12: false,
+    timeZone: "Asia/Kolkata" 
   });
 };
