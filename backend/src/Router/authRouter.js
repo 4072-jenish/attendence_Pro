0ce15e53e6291e9105d14888cc4729d4502d8be1
Jenish-Hariyani <150036720+4072-jenish@users.noreply.github.com>
2026-02-26 14:37:47 +0530

    Update authRouter.js

--- backend/src/Router/authRouter.js
+++ backend/src/Router/authRouter.js
@@ -4,7 +4,7 @@ const { loginUser, registerUser } = require('../Controller/authController');
 const authRouter = express.Router();
 
 authRouter.post('/regUser', registerUser);
-authRouter.use('/login', loginUser);
+authRouter.post('/login', loginUser);
 
 
 module.exports = authRouter;
